763db123eeacdaf15e13195bd0d0ecf51b9a3b41
anxhelo_shehu <57600286+anxheloshehu@users.noreply.github.com>
2025-12-16 08:01:48 +0100

    Add files via upload

---
+++ Week 6/krahasimi_njuton.m
@@ -0,0 +1,10 @@
+xx = pi/6;
+exact = sin(xx);
+
+perafrimi_para = double(subs(P_para, X, xx));
+perafrimi_pase = double(subs(P_pase, X, xx));
+perafrimi_qendrore = double(subs(P_qendrore, X, xx));
+
+disp('Metoda e Njutonit - Diferencat')
+disp('    Exacte        Para        Pase      Qendrore')
+disp([exact perafrimi_para perafrimi_pase perafrimi_qendrore])
